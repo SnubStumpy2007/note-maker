9351fba6e3f874e6722fc46691ef2e02bc4d2857
SnubStumpy2007 <joshua192007@gmail.com>
2023-07-21 19:30:50 -0400

    README Created

--- README.md
+++ README.md
@@ -1,8 +1,61 @@
-# Aknowlegements
-https://www.geeksforgeeks.org/how-to-build-note-taking-application-using-node-js/#
-https://medium.com/swlh/node-js-module-system-part-1-note-taking-app-78b642242370
-https://www.geeksforgeeks.org/express-js-app-post-function/#
-https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST
-https://expressjs.com/en/resources/middleware/body-parser.html
-https://github.com/jfisher396/express-note-taker
+## Simple Note-Taking Web Application
+
+This is a simple note-taking web application built using Node.js and Express.js. It allows users to create and view notes, and all notes are stored in a JSON file on the server.
+
+# Table of Contents
+
+Installation
+Usage
+Endpoints
+Technologies Used
+License
+
+# Installation
+
+Make sure you have Node.js installed on your machine.
+Clone this repository to your local machine or download the source code as a ZIP file.
+Navigate to the project directory using the terminal or command prompt.
+Run npm install to install the required dependencies.
+
+# Usage
+
+After installing the dependencies, start the application by running npm start or node index.js.
+The application will start, and you should see a message indicating that the server is listening on a specific port.
+Open your web browser and go to <http://localhost:3001>.
+
+# Endpoints
+
+POST /api/notes
+This endpoint allows users to create a new note. The request body should be in JSON format and include the title and text of the note. The server will generate a unique ID for the new note using the uuid library.
+
+GET /api/notes
+This endpoint returns all the notes currently stored on the server in JSON format.
+
+GET /notes
+This endpoint serves the HTML page for the note-taking interface. Users can view and manage their notes from this page.
+
+GET / (Catch-all)
+For any other URL that is not defined above, the server will serve the homepage HTML page. This ensures that the application always loads the correct HTML page, even if the URL is not recognized.
+
+# Technologies Used
+
+Node.js
+Express.js
+uuid (for generating unique IDs)
+HTML
+CSS
+JavaScript
+
+# License
+
+This project is licensed under the MIT License.
+
+# Sources
+
+<https://www.geeksforgeeks.org/how-to-build-note-taking-application-using-node-js/#>
+<https://medium.com/swlh/node-js-module-system-part-1-note-taking-app-78b642242370>
+<https://www.geeksforgeeks.org/express-js-app-post-function/#>
+<https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST>
+<https://expressjs.com/en/resources/middleware/body-parser.html>
+<https://github.com/jfisher396/express-note-taker>
 Tutor Jehyun Jung
--- server.js
+++ server.js
@@ -1,79 +1,87 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-//initalize app and define port number
+// Initialize the Express app and define the port number
 const app = express();
-// In many environments like Heroku, I can set a manual port number. https://stackoverflow.com/questions/18864677/what-is-process-env-port-in-node-js
 const PORT = process.env.PORT || 3001;
 
+// Load existing notes from the 'db.json' file into the 'notes' array
 const notes = require('./db/db.json');
 
-// Middlewear
+// Middleware
 // Parse data through JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-// app.gets
-
-
-
+// POST request to create a new note
 app.post('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if(err) {
-            console.log(err)
-            return res.status(500).json({error: 'Failed to read notes.'})
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ error: 'Failed to read notes.' });
         }
 
+        // Parse existing notes data
         const note = JSON.parse(data);
 
-        const newNotes =  {
+        // Create a new note object with a unique ID using 'uuidv4'
+        const newNote = {
             id: uuidv4(),
             title: req.body.title,
             text: req.body.text,
         };
 
-        note.push(newNotes);
+        // Add the new note to the 'note' array
+        note.push(newNote);
+
+        // Write the updated 'note' array back to the 'db.json' file
         fs.writeFile('./db/db.json', JSON.stringify(note), (err) => {
-            if(err) {
-                console.log(err)
-                return res.status(500).json({error: 'Failed to read notes.'})
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ error: 'Failed to write notes.' });
             }
 
-            res.json(newNotes)
-        })
-    })
-})
-
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if(err) {
-            console.log(err)
-            return res.status(500).json({error: 'Failed to read notes.'})
-        }
-
-        const note = JSON.parse(data);
-        res.json(note);
-    })
-    
+            // Respond with the newly created note in JSON format
+            res.json(newNote);
+        });
+    });
 });
 
+// GET request to retrieve all notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ error: 'Failed to read notes.' });
+        }
+
+        // Parse existing notes data
+        const note = JSON.parse(data);
+
+        // Respond with the 'note' array containing all notes in JSON format
+        res.json(note);
+    });
+});
+
+// GET request to serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// GET request to serve the notes.html file
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// Catch-all route (*) to redirect to the index.html file for any other request
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// Start the server and listen on the defined port
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
-   
-  });
+});
